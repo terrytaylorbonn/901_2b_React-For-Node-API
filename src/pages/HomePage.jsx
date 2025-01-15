9aec9f648bb82b2b982bf7f10fdbd480df53a874
T Taylor <terrytaylorbonn@gmail.com>
2025-01-15 14:08:58 -0500

    react for ch2b

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import Product from "../components/Product";
+import TableProduct from "../components/TableProduct";
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get("https://nine01-devtamin.onrender.com/api/products");
+      console.log(response.data)
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Link
+          to="/create"
+          className="inline-block mt-4 shadow-md bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer"
+        >
+          Create a Product
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
+        {isLoading ? (
+          "Loading"
+        ) : (
+          <>
+            {products.length > 0 ? (
+              <>
+                {products.map((product, index) => {
+                  return (
+                    <Product
+                      key={index}
+                      product={product}
+                      getProducts={getProducts}
+                    />
+                  );
+                })}
+              </>
+            ) : (
+              <div className="mt-4 bg-gray-800 text-white font-serif p-4">
+                There is no product
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      <TableProduct products={products} />
+    </div>
+  );
+};
+
+export default HomePage;
